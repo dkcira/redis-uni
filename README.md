28ed3306099302e2ab23c08d20bc8df0ec265a5c
Dorian Kcira <dkcira@gmail.com>
2019-06-19 22:57:19 -0700

    first version

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # redis-uni
 redis university
+
+
+https://hub.docker.com/_/redis
+
+docker run --name dredis -d redis:5.0.5-alpine
+
+or, better:
+docker-compose up -d
+
+docker exec -it dredis 'redis-cli'
+redis-cli> set hello world
+
+python3 helloworld.py
+
--- helloworld.py
+++ helloworld.py
@@ -2,8 +2,11 @@
 from redis import StrictRedis
 import os
 
-redis = StrictRedis(host=os.environ.get("REDIS_HOST", "localhost"),
+#localhost="localhost" # linux
+localhost="192.168.99.100" # mac
+
+redis = StrictRedis(host=os.environ.get("REDIS_HOST", localhost),
                     port=os.environ.get("REDIS_PORT", 6379),
                     db=0)
 
-print redis.get("hello")
+print(redis.get("hello"))
